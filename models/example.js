cf00413e3fce8d40b6b317cbe25022d24534ba6a
Will Mangimelli <willwinman@gmail.com>
2019-06-22 09:52:41 -0500

    Added user model. Added createUser functionality to index.js API. Added route for create new user in apiRoutes.

--- models/example.js
+++ models/example.js
@@ -1,7 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
   var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
+    description: DataTypes.TEXT,
+    mood: {
+      type: DataTypes.INTEGER,
+      defaultValue: 3,
+      validate: {
+        len: {
+          args: [1, 5],
+          msg:
+            "Mood value should be between 1 and 5, where 5 is feeling amazing."
+        }
+      }
+    }
   });
   return Example;
 };
