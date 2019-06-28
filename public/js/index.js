c5e3f56535787f1961079c011b1f46694552f78b
Nicholas Merritt <nicholasmerritt1994@gmail.com>
2019-06-27 20:39:45 -0500

    Added logic to get my event handlers working

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,16 @@
+$(function(){
+
 // Get references to page elements
 //var $submitBtn2 = $("#neutral");
 var $submitid = $("#submitbtn");
-var $moodid = $("#mood");
-var $exerciseid = $("#exercise");
-var $sad = $(".sad");
-var $neutral = $(".neutral");
-var $happy = $(".happy");
-var $treadmill = $(".treadmill");
-var $weights = $(".weights");
-var $bike = $(".bike");
+var $mood = $("#mood");
+var $activity = $("#activity");
+//var $sad = $(".sad");
+//var $neutral = $(".neutral");
+//var $happy = $(".happy");
+//var $treadmill = $(".treadmill");
+//var $weights = $(".weights");
+//var $bike = $(".bike");
 
 
 
@@ -20,13 +22,13 @@ var API = {
         "Content-Type": "application/json"
       },
       type: "POST",
-      url: "/api/mood",
+      url: "api/mood",
       data: JSON.stringify(example)
     });
   },
   getExamples: function() {
     return $.ajax({
-      url: "/api/mymood",
+      url: "api/mymood",
       type: "GET"
     });
   },
@@ -82,7 +84,7 @@ var refreshExamples = function() {
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
+/*var handleFormSubmit = function(event) {
   event.preventDefault();
 
   var day = {
@@ -93,26 +95,69 @@ var handleFormSubmit = function(event) {
   API.saveExample(day).then(function() {
     refreshExamples();
   });
-
+  console.log(day)
   //$submitBtn.val("");
 };
 
-var selectmood = function(event) {
-  event.preventDefault();
-  this.setAttribute("id", "mood");
+var selectSad = function(event) {
+  //event.preventDefault();
+  //element.setAttribute("id", "mood");
+  setAttribute("id", "mood")
+};
+
+var selectNeutral = function(event) {
+  //event.preventDefault();
+  //element.setAttribute("id", "mood");
+  $(this).setAttribute("id", "mood")
+};
+
+var selectHappy = function(event) {
+  //event.preventDefault();
+  //element.setAttribute("id", "mood");
+  document.getElementsByClassName("happy")[0].setAttribute("id", "mood");
 };
 
 var selectexercise = function(event) {
-  event.preventDefault();
+  //event.preventDefault();
   this.setAttribute("id", "exercise");
-};
+};*/
 
 
 // Add event listeners to the submit button
-$sad.on("click", selectmood);
-$neutral.on("click", selectmood);
-$happy.on("click", selectmood);
+/*$(".sad").click(function() {
+  setAttribute("id", "exercise");
+});*/
+//$(".sad").on("click", function(event){
+//  this.setAttribute("id", "mood");;
+//});
+
+var handleFormSubmit = function(event) {
+  event.preventDefault();
+
+  var example = {
+    mood: $mood.val().trim(),
+    activity: $activity.val().trim()
+  };
+  console.log(example)
+
+  
+
+  API.saveExample(example).then(function() {
+    refreshExamples();
+  });
+
+  $mood.val("");
+  $activity.val("");
+  console.log(example)
+};
+
+
+
+
+/*$neutral.on("click", selectNeutral);
+$happy.on("click", selectHappy);
 $treadmill.on("click", selectexercise);
 $weights.on("click", selectexercise);
-$bike.on("click", selectexercise);
+$bike.on("click", selectexercise);*/
 $submitid.on("click", handleFormSubmit);
+});
