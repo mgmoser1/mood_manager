6b3c563138f93d5a0e67748da08dfe5d53acc7c7
Nicholas Merritt <nicholasmerritt1994@gmail.com>
2019-06-28 21:28:38 -0500

    attached my event handlers to the mood.handlebars and exercise.handlebars

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,16 @@
 $(function(){
 
 // Get references to page elements
-//var $submitBtn2 = $("#neutral");
 var $submitid = $("#submitbtn");
 var $mood = $("#mood");
 var $activity = $("#activity");
-//var $sad = $(".sad");
-//var $neutral = $(".neutral");
-//var $happy = $(".happy");
-//var $treadmill = $(".treadmill");
-//var $weights = $(".weights");
-//var $bike = $(".bike");
+var $submitbtntr = $("#submitbtntr")
+var $submitbtnw = $("#submitbtnw")
+var $submitbtnb = $("#submitbtnb")
+var $submitbtnh = $("#submitbtnh")
+var $submitbtnn = $("#submitbtnn")
+var $submitbtns = $("#submitbtns")
+
 
 
 
@@ -49,9 +49,7 @@ var API = {
 localStorage = window.localStorage;
 
 if (!localStorage.getItem("trackRabbit")) {
-  //const userID = Math.floor(Math.random() * 10000);
-  //localStorage.setItem("trackRabbit", userID);
-  //create new user
+  
   API.createUser();
 }
 
@@ -63,7 +61,7 @@ var refreshExamples = function() {
     var $examples = data.map(function(example) {
       var $a = $("<a>")
         .text(example.text)
-        .attr("href", "/example/" + example.id);
+        .attr("href", "api/mymood" + example.id);
 
       var $li = $("<li>")
         .attr({
@@ -82,68 +80,22 @@ var refreshExamples = function() {
   });
 };
 
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-/*var handleFormSubmit = function(event) {
-  event.preventDefault();
 
-  var day = {
-    mood: $moodid.val(),
-    exercise: $exerciseid.val()
-  };
-
-  API.saveExample(day).then(function() {
-    refreshExamples();
-  });
-  console.log(day)
-  //$submitBtn.val("");
-};
-
-var selectSad = function(event) {
-  //event.preventDefault();
-  //element.setAttribute("id", "mood");
-  setAttribute("id", "mood")
-};
-
-var selectNeutral = function(event) {
-  //event.preventDefault();
-  //element.setAttribute("id", "mood");
-  $(this).setAttribute("id", "mood")
-};
-
-var selectHappy = function(event) {
-  //event.preventDefault();
-  //element.setAttribute("id", "mood");
-  document.getElementsByClassName("happy")[0].setAttribute("id", "mood");
-};
-
-var selectexercise = function(event) {
-  //event.preventDefault();
-  this.setAttribute("id", "exercise");
-};*/
-
-
-// Add event listeners to the submit button
-/*$(".sad").click(function() {
-  setAttribute("id", "exercise");
-});*/
-//$(".sad").on("click", function(event){
-//  this.setAttribute("id", "mood");;
-//});
 
 var handleFormSubmit = function(event) {
   event.preventDefault();
 
   var example = {
-    mood: $mood.val().trim(),
+    //mood: $mood.val().trim(),
     activity: $activity.val().trim()
   };
-  console.log(example)
+  
 
   
 
   API.saveExample(example).then(function() {
-    refreshExamples();
+    //refreshExamples();
+    console.log("It worked")
   });
 
   $mood.val("");
@@ -154,10 +106,11 @@ var handleFormSubmit = function(event) {
 
 
 
-/*$neutral.on("click", selectNeutral);
-$happy.on("click", selectHappy);
-$treadmill.on("click", selectexercise);
-$weights.on("click", selectexercise);
-$bike.on("click", selectexercise);*/
 $submitid.on("click", handleFormSubmit);
+$submitbtntr.on("click", handleFormSubmit);
+$submitbtnw.on("click", handleFormSubmit);
+$submitbtnb.on("click", handleFormSubmit);
+$submitbtnh.on("click", handleFormSubmit);
+$submitbtnn.on("click", handleFormSubmit);
+$submitbtns.on("click", handleFormSubmit);
 });
